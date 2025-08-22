60d53a53fffb649bcc03ca5a0080c0212f0c1dfa
Giuliano Maciel <giulianomaciellopes@gmail.com>
2025-08-22 13:24:36 -0300

    preparando para deploy

--- src/cronJobs/reportday.ts
+++ src/cronJobs/reportday.ts
@@ -2,8 +2,8 @@ import cron from "node-cron";
 import { reportDayCreate } from "@/services/cronjobs/reportcreatedayjobs.js";
 // em sp 23:59 em producao 2:59
 export function starReportDayJob() {
-  cron.schedule("59 23 * * *", async () => {
+  cron.schedule("59 2 * * *", async () => {
     await reportDayCreate();
-    console.log("Executando tarefa diária à 9:15 teste");
+    console.log("");
   });
 }
